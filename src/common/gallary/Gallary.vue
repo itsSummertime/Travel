af9d1442ec11e75eed8f0b63c0bb3395dff431e8
Shirley Gao <shirleygao1112@gmail.com>
2022-04-08 20:32:36 +0800

    ajax

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -3,7 +3,7 @@
     <div class="wrapper">
       <swiper :options="swiperOption">
         <swiper-slide v-for="(item, index) of imgs" :key="index">
-          <img class="gallary-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg">
+          <img class="gallary-img" :src="item">
         </swiper-slide>
         <div class="swiper-pagination"  slot="pagination"></div>
       </swiper>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -41,10 +41,10 @@ export default {
       }
     }
   },
-  activated () {
+  mounted () {
     window.addEventListener('scroll', this.handleScroll)
   },
-  deactivated () {
+  destroyed () {
     window.removeEventListener('scroll', this.handleScroll)
   }
 
